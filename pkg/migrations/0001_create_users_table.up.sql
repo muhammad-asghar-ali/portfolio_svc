d429503982130b3485b4ebdce83f93c57741483d
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-02-17 00:24:15 +0500

    chore(): add email contraints in sql table

--- pkg/migrations/0001_create_users_table.up.sql
+++ pkg/migrations/0001_create_users_table.up.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -8,3 +7,6 @@ CREATE TABLE IF NOT EXISTS users (
     signup_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+ALTER TABLE public.users
+  -- taken from OSWAP https://owasp.org/www-community/OWASP_Validation_Regex_Repository
+  ADD CONSTRAINT users_email_check CHECK (email ~* '^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$');
